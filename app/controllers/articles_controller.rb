624981b2f3039835516981182f94adabf1b89110
tenhold <tenhold@gmail.com>
2023-09-24 11:24:46 -0500

    add 9.2 comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -35,6 +35,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path, status: :see_other # 303 see_other
+  end
+
   private
 
   def article_params
