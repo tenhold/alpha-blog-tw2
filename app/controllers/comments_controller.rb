cd4dabff9cadaa227ad1b2dce2ad2d9f64907842
tenhold <tenhold@gmail.com>
2023-09-24 11:45:17 -0500

    add 9.3 adding concerns

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,13 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    debugger
+
     redirect_to article_path(@article)
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body, :status)
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,11 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 
+  VALID_STATUSES = %w(public private archived)
+
+  validates :stauts, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
