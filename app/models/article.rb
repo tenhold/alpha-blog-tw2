2336aa347c0c64b46aafddb03e9510a1057842ab
tenhold <tenhold@gmail.com>
2023-10-05 21:34:28 -0500

    add to 10 deleting comments.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,14 +1,8 @@
 class Article < ApplicationRecord
+  include Visible
+
   has_many :comments
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
-
-  VALID_STATUSES = %w(public private archived)
-
-  validates :stauts, inclusion: { in: VALID_STATUSES }
-
-  def archived?
-    status == 'archived'
-  end
 end
